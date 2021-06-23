198f52affe1b417ee0c89e24746897bd004f46d6
ibrahim-ctrl <rizky.si@students.amikom.ac.id>
2021-06-23 17:33:52 +0900

    update lagi

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-app.get('/*', (req, res)=>{
-    let name = req.params
+const express = require('express')
+const { get } = require('http')
+
+const app = express()
+const port = 3000
+
+app.get('/ibrahim-ctrl', (req, res)=>{
+    res.redirect('https://ibrahim-ctrl.github.io/')
 })
